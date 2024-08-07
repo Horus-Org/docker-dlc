1db18e8c9936cfdf103cedd472766d10b98f4d3e
223880 <rafael.segala45@gmail.com>
2024-08-07 07:14:27 -0300

    Add beta 1 version

---
+++ target/debug/deps/Docker_DLC-3e96288d391abec4.d
@@ -0,0 +1,5 @@
+/sysroot/home/rafael/Documents/DLC docker/docker-dlc/target/debug/deps/libDocker_DLC-3e96288d391abec4.rmeta: src/main.rs
+
+/sysroot/home/rafael/Documents/DLC docker/docker-dlc/target/debug/deps/Docker_DLC-3e96288d391abec4.d: src/main.rs
+
+src/main.rs:
