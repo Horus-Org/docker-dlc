5fef6f7d4467ecce8d509c8d25eaa5a0e175a141
223880 <rafael.segala45@gmail.com>
2024-09-02 06:51:39 -0300

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Import the DLCMessage struct from the DLCMessage module
 extern crate dlc_messages;
 
+use dlc_messages::segmentation::segment_reader::SegmentReader;
 use dlc_messages::{Channel, OracleMessages, SegmentReader};
 // Define DLC Builder
 #[warn(dead_code)]
