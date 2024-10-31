061efd52159045da5bae71ca8f8e35d56778b078
223880 <rafael.segala45@gmail.com>
2024-10-31 06:39:49 -0300

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 // Import the dlc_messages crate
 use dlc_messages::Message;
-use ddk_messages::Message;
-
+use dlc_messages::Message;
 // Define DLCBuilder struct
 #[warn(dead_code)]
 pub struct DLCBuilder {
