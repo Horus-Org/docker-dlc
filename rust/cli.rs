8a1819f0cc72b7ac191a360cb432e73af0796199
22388o <22388o@users.noreply.github.com>
2024-12-16 06:39:06 -0300

    Initial CLI

---
+++ rust/cli.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Path to the file to read
+    #[clap(parse(from_os_str))]
+    pub
+    path: PathBuf,
+}
